25d038e74b1e5c77fb8f348505c9088ad17d2630
alloyking1 <alloyking1@gmail.com>
2022-10-19 18:21:00 +0100

    setup/configured route service provider for differenct route files and created route files

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,14 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware(['web'])
+                ->group(base_path('routes/job/job.php'));
+            Route::middleware(['web'])
+                ->group(base_path('routes/profile/profile.php'));
+
+            Route::middleware(['web'])
+                ->group(base_path('routes/company/company.php'));
         });
     }
 
--- routes/profile/profile.php
+++ routes/profile/profile.php
@@ -13,4 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::middleware(['web'])->prefix('user')->group(function () {
+    Route::get('/test/user', function () {
+        dump('yaya');
+    });
+});
